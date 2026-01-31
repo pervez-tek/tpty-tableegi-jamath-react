1f06c40b7725354c55ee48e4a069db6055dc9e0a
unknown <pervez.tek@gmail.com>
2026-01-31 20:51:22 +0530

    Few Masjids added dummy

--- src/services/HalkaMasjidDropDown.jsx
+++ src/services/HalkaMasjidDropDown.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-import { initialMasjidsList1 } from "../components/dummyMasjidsData";
+//import { initialMasjidsList1 } from "../components/dummyMasjidsData";
 import { toast } from "react-toastify";
 
 
